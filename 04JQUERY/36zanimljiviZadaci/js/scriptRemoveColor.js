8cf27d5e2b7993ed878a784850b4de9af9371f1d
SasaAndjelkovic <sandjelkovickg@gmail.com>
2022-10-01 20:03:49 +0200

    Js resenja da, Jquery jos ne

---
+++ 04JQUERY/36zanimljiviZadaci/js/scriptRemoveColor.js
@@ -0,0 +1,20 @@
+
+//JS resenje
+// let boja = document.getElementById("colorSelect");
+// // console.log(boja);
+// // console.log(i);
+// let dugme = document.getElementById("dugme");
+
+// dugme.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     // var x = document.getElementById("colorSelect");
+//     console.log(boja);
+//     console.log(boja[boja.selectedIndex].value);
+//     boja.remove(boja.selectedIndex);
+// });
+
+//JQuery resenje
+$("#dugme").click(function (e) {
+    e.preventDefault();
+    $("#colorSelect").remove($("#colorSelect").selectedIndex);
+});
