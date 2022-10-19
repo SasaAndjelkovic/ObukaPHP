5912c8e4df592cb560e8076c35d778ae9e55b69b
SasaAndjelkovic <sandjelkovickg@gmail.com>
2022-10-19 21:05:40 +0200

    php html

---
+++ 05PHP/06PHP&HTML/funkcije.php
@@ -0,0 +1,53 @@
+<!-- 
+
+// include "podaci.php";
+
+// function vratiPDV($cena)
+// {
+//     $pdv = $cena * 0.2;
+//     return $pdv;
+// } 
+
+// function vratiRabat($kolicina)
+// {
+//     // $rabat = 0;
+//     // if ($kolicina < 40) {
+//     //     $rabat = 2;
+//     // } else if ($kolicina < 50) {
+//     //     $rabat = 3;
+//     // } else {
+//     //     $rabat = 5;
+//     // }
+//     // return $rabat;
+
+//     // if ($kolicina < 40) {
+//     //     return 2;
+//     // } 
+//     // if ($kolicina < 50) {
+//     //     return 3;
+//     // } 
+//     //     return 5;
+
+//     if ($kolicina < 40) return 2;
+//     if ($kolicina < 50) return 3;
+//     return 5;
+// }
+
+// function vratiIznos($cena, $kolicina)
+// {
+//     $iznosPDV = vratiPDV($cena);
+//     $cenaSaPDV = $cena + $iznosPDV;
+//     $iznos = $cenaSaPDV * $kolicina;
+//     $iznosRabata = $iznos * vratiRabat($kolicina) / 100;
+//     return $iznos - $iznosRabata;
+// }
+
+// function vratiUkupno()
+// {
+//     global $proizvodi;
+//     $suma = 0; //neutralna vrednost
+//     foreach ($proizvodi as $pr) {
+//         $suma += vratiIznos($pr["cena"], $pr["kolicina"]);
+//     }
+//     return $suma;
+// } 
---
+++ 05PHP/06PHP&HTML/proizvodi.php
@@ -0,0 +1,71 @@
+<?php
+
+include "podaci.php";
+// include "funkcije.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>PHP&HTML</title>
+</head>
+
+<body>
+    <h1>Prodavnica elektronike</h1>
+    <h3>Narudzbenica</h3>
+    <table>
+        <thead>
+            <th>Naziv</th>
+            <th>Jedinicna cena</th>
+            <th>Kolicina</th>
+            <th>PDV</th>
+            <th>Rabat</th>
+            <th>Ukupan iznos</th>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($prozvodi as $pr) :
+            ?>
+                <tr>
+                    <td><?php echo $pr["naziv"] ?></td>
+                    <td><?php echo $pr["cena"] ?></td>
+                    <td><?php echo $pr["kolicina"] ?></td>
+                    <td><?php echo vratiPDV($pr["cena"]) ?></td>
+                    <td><?php echo vratiRabat($pr["kolicina"]) ?></td>
+                    <td><?php echo vratiIznos($pr["cena"], $pr["kolicina"]) ?></td>
+                </tr>
+            <?php
+            endforeach;
+            ?>
+            <!-- <tr>
+                <td>Laptop</td>
+                <td>900</td>
+                <td>65</td>
+            </tr> -->
+            <!-- <tr>
+                <td>Monitor</td>
+                <td>350</td>
+                <td>35</td>
+            </tr>
+            <tr>
+                <td>Tastatura</td>
+                <td>200</td>
+                <td>47</td>
+            </tr> -->
+        </tbody>
+        <tfoot>
+            <td colspan="5">Ukupno:</td>
+            <td><?php
+                echo vratiUkupno()
+                ?></td>
+        </tfoot>
+    </table>
+</body>
+
+</html>
