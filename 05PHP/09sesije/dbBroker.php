5073a2e65c79a2c159b532c7ee5b80ee03bdb03c
SasaAndjelkovic <sandjelkovickg@gmail.com>
2022-10-26 19:30:02 +0200

    dodat broker

---
+++ 05PHP/09sesije/dbBroker.php
@@ -0,0 +1,14 @@
+<?php
+
+$baza = array(
+    array(
+        "id" => 1,
+        "username" => "admin",
+        "password" => "admin123"
+    ),
+    array(
+        "id" => 2,
+        "username" => "Petar",
+        "password" => "petar123"
+    )
+);
--- 05PHP/09sesije/index2.php
+++ 05PHP/09sesije/index2.php
@@ -1,3 +1,9 @@
+<?php
+
+require "dbBroker.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
