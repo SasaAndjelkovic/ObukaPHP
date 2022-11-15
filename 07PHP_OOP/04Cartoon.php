de69286d9952233f86bc6269f86db4dbc0444230
SasaAndjelkovic <sandjelkovickg@gmail.com>
2022-11-15 23:50:36 +0100

    uradjen zadatak u osnovi

---
+++ 07PHP_OOP/04Cartoon.php
@@ -0,0 +1,151 @@
+<?php
+
+// cartoon caracter can walk and jump
+// based on their type they make different sounds
+// The Simpsons have their catch phrase, as well as Loonie Tunes and Disney characters
+
+// SchoolKids are type of Simpsons, Rabbits are part of Loonie Tunes 
+//and princesses are part of the Disneys
+
+// Learning is reading and memorizing
+// Playing is running
+// Going back to life is standingUp and shinning
+
+// SchoolKids can play and learn, Rabits can play, princesses can do all
+
+// make Bart, Bugs Bunny and Cinderella as instances
+
+abstract class CartoonCaracter {
+    public string $name;
+    function setName($name) {
+        $this->name = $name;
+    }
+    function walk(){}
+    function jump(){}
+    abstract function makeSound();
+}
+
+class Simpsons extends CartoonCaracter {
+    function catchPhrase(){
+        echo $this->name . ": Auch!";
+    }
+
+	public function makeSound() {
+	}
+}
+
+class LoonieTunes extends CartoonCaracter {
+    function catchPhrase(){}
+	
+	public function walk() {
+	}
+	
+	public function jump() {
+	}
+	
+	public function makeSound() {
+	}
+}
+
+class Disney extends CartoonCaracter {
+    function catchPhrase(){}
+	
+	public function walk() {
+	}
+	
+	public function jump() {
+	}
+
+	public function makeSound() {
+        echo $this->name . " sings: 'A Dream Is A Wish Your Heart Makes'";
+	}
+}
+
+interface Learn {
+    function read();
+    function memorize();
+}
+
+interface Play {
+    function running();
+}
+
+interface BackToLife {
+    function standUp();
+    function shining();
+}
+
+class SchoolKids extends Simpsons implements Learn, Play {
+	public function read() {
+	}
+	
+	public function memorize() {
+	}
+	
+	public function running() {
+	}
+	
+	public function walk() {
+	}
+	
+	public function jump() {
+	}
+}
+
+class Rabbits extends LoonieTunes implements Play {
+
+	public function running() {
+        echo $this->name . " is running away from Milica"; 
+	}
+	
+	public function walk() {
+	}
+	
+	public function jump() {
+	}
+}
+
+class Princesses extends Disney implements Learn, Play, BackToLife {
+    
+	public function read() {
+	}
+	
+	public function memorize() {
+	}
+	
+	public function running() {
+	}
+
+	public function standUp() {
+	}
+	
+	public function shining() {
+	}
+	
+	public function walk() {
+	}
+	
+	public function jump() {
+	}
+}
+
+$schoolKid = new SchoolKids();
+$schoolKid->setName("Bart");
+$schoolKid->catchPhrase();
+echo "<br>";
+$rubbit = new Rabbits();
+$rubbit->setName("Bugs Bunny");
+$rubbit->running();
+echo "<br>";
+
+$princesse = new Princesses();
+$princesse->setName("Cinderella");
+$princesse->makeSound();
+
+
+
+
+
+
+
+
