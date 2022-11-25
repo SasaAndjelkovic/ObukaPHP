df951c4e53b0fd4363fc3ff2d4f6213332362459
SasaAndjelkovic <sandjelkovickg@gmail.com>
2022-11-25 12:22:58 +0100

    static vs nonstatic

--- 07PHP_OOP/06shop/Pensioner.php
+++ 07PHP_OOP/06shop/Pensioner.php
@@ -3,6 +3,19 @@
 class Pensioner extends Customer {
 
     public static float $discount = 0.2;
+
+    // T - property: static -> method: static
+    // Pensioner::getDiscount(); ne moze $p = new Pensioner() // $p->getDiscount()
+    // public static function getDiscount() }
+    //    return self::$discount;
+    // }
+    // T - property: static -> method: non static (primer u nastavku)
+    // F - property: non static -> method: static 
+    // public static function getDiscount() }
+    //    $this->money; -- ovo ne moze, this se koristi za objekat, instancu
+    //    return self::$discount;
+    // }
+    // T - property: non static -> method: non static (uobicajeno)
    
     public function buy(Order $o){
         if($this->money < $o->getTotalPrice() * (1 - self::$discount))
