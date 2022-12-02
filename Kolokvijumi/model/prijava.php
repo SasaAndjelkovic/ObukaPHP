dad102dd42a120eb20f521c37e9bf3382a7c5b08
SasaAndjelkovic <sandjelkovickg@gmail.com>
2022-12-02 13:35:09 +0100

    funkcija dodaj

--- Kolokvijumi/model/prijava.php
+++ Kolokvijumi/model/prijava.php
@@ -16,9 +16,18 @@ class Prijava {
         $this->datum = $datum;
     }
 
+    //prikazi sve
     public static function prikaziSve(mysqli $conn){
         $query = "SELECT *
                   FROM prijave";
+
+        return $conn->query($query);
+    }
+
+    //kreiranje/dodavanje nove prijave
+    public static function dodaj(Prijava $prijava, mysqli $conn) {
+        $query = "INSERT INTO prijave(predmet, katedre, sala, datum) 
+                  VALUES ('$prijava->predmet', '$prijava->katedra', '$prijava->sala', '$prijava->datum')";
 
         return $conn->query($query);
     }
