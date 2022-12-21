3144488182779d738fa07ecfb2f4831746b9fa3e
SasaAndjelkovic <sandjelkovickg@gmail.com>
2022-12-21 17:14:12 +0100

    zato sto ima mnogo klasi Student, prepravljeno na StudentDom

--- 20Projekti/04DomaciFakultet/model/student.php
+++ 20Projekti/04DomaciFakultet/model/student.php
@@ -1,6 +1,6 @@
 <?php
 
-interface Student{
+interface StudentDom{
     public function getStatus(); //funkcija koja ce se kroz osn/mast/dok 
     public function setIndeks($indeks); 
     public function getIndeks(); 
