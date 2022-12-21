e9dc3d295f1a2e92c79b4562f77e9e22f1373e5f
SasaAndjelkovic <sandjelkovickg@gmail.com>
2022-12-21 17:19:39 +0100

    mstudent i dstudent

---
+++ 20Projekti/04DomaciFakultet/model/mstudent.php
@@ -0,0 +1,72 @@
+<?php
+
+class MStudent extends Korisnik implements StudentDom{
+
+    private $status = "master";
+    private $indeks;
+
+    public function __construct($ime,$prezime,$email,$sifra,$jmbg,$telefon,$tip, $indeks)
+    {
+        parent::__construct($ime, $prezime, $email, $sifra, $jmbg, $telefon, $tip);//pristupamo funkcijama u roditelju
+        $this->indeks = $indeks;
+    }
+	public function getStatus() {
+        return $this->status;
+	}
+	public function setIndeks($indeks) {
+        $this->indeks = $indeks;
+	}
+	public function getIndeks() {
+        return $this->indeks;
+	}
+
+	public function getIme() {
+        return $this->ime;
+	}
+	
+	public function setIme($imekorisnika) {
+        $this->ime = $imekorisnika;
+	}
+	
+	public function getPrezime() {
+        return $this->prezime;
+	}
+	
+	public function setPrezime($prezimekorisnika) {
+        $this->prezime = $prezimekorisnika;
+	}
+	
+	public function getEmail() {
+        return $this->email;
+	}
+	
+	public function setEmail($emailkorisnika) {
+        $this->email = $emailkorisnika;
+	}
+	
+	public function getSifra() {
+        return $this->sifra;
+	}
+	
+	public function setSifra($sifrakorisnika) {
+        $this->sifra = $sifrakorisnika;
+	}
+	
+	public function getTelefon() {
+        return $this->telefon;
+	}
+	
+	public function setTelefon($telefonkorisnika) {
+        $this->telefon= $telefonkorisnika;
+	}
+
+	public function getTip() {
+        return $this->tip;
+	}
+	
+	public function setTip($tipkorisnika) {
+        $this->tip = $tipkorisnika;
+	}
+}
+
+?>
