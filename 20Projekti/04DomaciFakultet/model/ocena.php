d80c6087e47b4a16e5996477b37c0328b0e47df6
SasaAndjelkovic <sandjelkovickg@gmail.com>
2022-12-25 15:43:51 +0100

    fajl ocena.php

---
+++ 20Projekti/04DomaciFakultet/model/ocena.php
@@ -0,0 +1,66 @@
+<?php
+
+class Ocena{
+
+    protected StudentDom $student;
+    protected Predmet $predmet;
+    protected Profesor $profesor;
+    protected int $ocena;
+    protected string $datum;
+
+    public function __construct(StudentDom $student,Predmet $predmet,Profesor $prof, int $ocena, string $dat)
+    {
+        $this->student = $student;
+        $this->predmet = $predmet;
+        $this->profesor = $prof;
+        $this->ocena = $ocena;
+        $this->datum = $dat;
+    }
+
+	public function getStudent(): StudentDOm {
+		return $this->student;
+	}
+	
+	public function getPredmet(): Predmet {
+		return $this->predmet;
+	}
+	
+	public function getProfesor(): Profesor {
+		return $this->profesor;
+	}
+	
+	public function getOcena(): int {
+		return $this->ocena;
+	}
+	
+	public function getDatum(): string {
+		return $this->datum;
+	}
+
+	public function setStudent(StudentDom $student): self {
+		$this->student = $student;
+		return $this;
+	}
+	
+	public function setPredmet(Predmet $predmet): self {
+		$this->predmet = $predmet;
+		return $this;
+	}
+	
+	public function setProfesor(Profesor $profesor): self {
+		$this->profesor = $profesor;
+		return $this;
+	}
+	
+	public function setOcena(int $ocena): self {
+		$this->ocena = $ocena;
+		return $this;
+	}
+	
+	public function setDatum(string $datum): self {
+		$this->datum = $datum;
+		return $this;
+	}
+}
+
+?>
