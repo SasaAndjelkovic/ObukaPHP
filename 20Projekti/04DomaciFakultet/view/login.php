06bb06421865e4bfb21f87527b59a73994de755a
SasaAndjelkovic <sandjelkovickg@gmail.com>
2022-12-25 19:52:18 +0100

    login forma

---
+++ 20Projekti/04DomaciFakultet/view/login.php
@@ -0,0 +1,5 @@
+<form action="" method="post">
+    <input type="text" placeholder="email" id="email" name="email">
+    <input type="text" placeholder="sifra" id="sifra" name="sifra">
+    <input type="submit" value="Loguj se" id="login" name="login">
+</form>
