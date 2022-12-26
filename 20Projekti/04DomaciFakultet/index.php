66eeac8ac3379e084eb1ba3870d2c4adc11bf242
SasaAndjelkovic <sandjelkovickg@gmail.com>
2022-12-26 19:14:43 +0100

    ciscenje index.php u loaddata...

--- 20Projekti/04DomaciFakultet/index.php
+++ 20Projekti/04DomaciFakultet/index.php
@@ -9,77 +9,81 @@ include "model/administrator.php";
 include "model/predmet.php";
 include "model/ocena.php";
 include "controler/controler.php";
+include "loaddata.php";
+
 
 include "view/login.php";
 
-$nizkorisnika = [];
+// sve je prebaceno u loaddata.php
 
-$so = new OStudent("tamara", "naumovic", "tamara@elab.rs", "tamara123", "1234567891234", "0662581477", "student", "2020/1005");
-$sm = new MStudent("petar", "lukovac", "petar@elab.rs", "petar123", "1231234891234", "0661281477", "student", "2020/2005");
-$sd = new DStudent("aleksa", "miletic", "aleksaa@elab.rs", "aleksa123", "1234561231234", "0662123477", "student", "2020/3005");
+// $nizkorisnika = [];
 
-$prof = new Profesor("zorica", "bogdanovic", "zorica@elab.rs", "zorica123", "7894561231234", "0662123789", "profesor");
+// $so = new OStudent("tamara", "naumovic", "tamara@elab.rs", "tamara123", "1234567891234", "0662581477", "student", "2020/1005");
+// $sm = new MStudent("petar", "lukovac", "petar@elab.rs", "petar123", "1231234891234", "0661281477", "student", "2020/2005");
+// $sd = new DStudent("aleksa", "miletic", "aleksaa@elab.rs", "aleksa123", "1234561231234", "0662123477", "student", "2020/3005");
 
-Kontroler::ulogujKorisnika($prof);
-// Kontroler::ulogujKorisnika($so);
+// $prof = new Profesor("zorica", "bogdanovic", "zorica@elab.rs", "zorica123", "7894561231234", "0662123789", "profesor");
 
-array_push($nizkorisnika, $so);
-array_push($nizkorisnika, $sm);
-array_push($nizkorisnika, $sd);
-array_push($nizkorisnika, $prof);
+// Kontroler::ulogujKorisnika($prof);
+// // Kontroler::ulogujKorisnika($so);
+
+// array_push($nizkorisnika, $so);
+// array_push($nizkorisnika, $sm);
+// array_push($nizkorisnika, $sd);
+// array_push($nizkorisnika, $prof);
+
+// // echo "<br>";
+// // print_r($nizkorisnika);
+
+// $nizstudenata = [$so, $sm, $sd];
+// // Kontroler::prikaziStudente(...$nizstudenata);
+
+// if(isset($_POST["login"])){
+//     $email = $_POST['email'];
+//     $sifra = $_POST['sifra'];
+//     foreach($nizkorisnika as $kr){
+//         if($kr->getEmail() == $email && $kr->getSifra()==$sifra){
+//             Kontroler::ulogujKorisnika($kr);  
+//         }
+//     }
+// }
+
+// // sreda
+// $nizpredmeta1 = [];
+// $nizpredmeta2 = [];
+// $nizocena = [];
+
+// $p1 = new Predmet("PHP", "php2022", "osnovne");
+// $p2 = new Predmet("JAVA", "java2022", "master");
+// $p3 = new Predmet("Blockchain", "bc2022", "doktorske");
+
+// array_push($nizpredmeta1, $p1);
+// array_push($nizpredmeta2, $p2);
+// array_push($nizpredmeta1, $p3);
+
+// $prof->setNizPredmeta($nizpredmeta1);
+
+// $p1->setSpisakProfesora([$prof]);
+// $p3->setSpisakProfesora([$prof]);
+// $p1->setSpisakStudenata([$so]);
+// $p2->setSpisakStudenata([$sm]);
+// $p3->setSpisakStudenata([$sd]);
+
+// echo "<br> lista predmeta1: ";
+// foreach($nizpredmeta1 as $pred):
+//     echo "<br>Predmet: ".$pred->getNaziv();
+//     echo "<br> Studenti <br>";
+//     print_r($pred->getSpisakStudenata());
+// endforeach;
+
+// echo "<br> lista predmeta2: ";
+// foreach($nizpredmeta2 as $pred):
+//     echo "<br>Predmet: ".$pred->getNaziv();
+//     echo "<br> Studenti <br>";
+//     print_r($pred->getSpisakStudenata());
+// endforeach;
 
 // echo "<br>";
-// print_r($nizkorisnika);
 
-$nizstudenata = [$so, $sm, $sd];
-// Kontroler::prikaziStudente(...$nizstudenata);
-
-if(isset($_POST["login"])){
-    $email = $_POST['email'];
-    $sifra = $_POST['sifra'];
-    foreach($nizkorisnika as $kr){
-        if($kr->getEmail() == $email && $kr->getSifra()==$sifra){
-            Kontroler::ulogujKorisnika($kr);  
-        }
-    }
-}
-
-// sreda
-$nizpredmeta1 = [];
-$nizpredmeta2 = [];
-$nizocena = [];
-
-$p1 = new Predmet("PHP", "php2022", "osnovne");
-$p2 = new Predmet("JAVA", "java2022", "master");
-$p3 = new Predmet("Blockchain", "bc2022", "doktorske");
-
-array_push($nizpredmeta1, $p1);
-array_push($nizpredmeta2, $p2);
-array_push($nizpredmeta1, $p3);
-
-$prof->setNizPredmeta($nizpredmeta1);
-
-$p1->setSpisakProfesora([$prof]);
-$p3->setSpisakProfesora([$prof]);
-$p1->setSpisakStudenata([$so]);
-$p2->setSpisakStudenata([$sm]);
-$p3->setSpisakStudenata([$sd]);
-
-echo "<br> lista predmeta1: ";
-foreach($nizpredmeta1 as $pred):
-    echo "<br>Predmet: ".$pred->getNaziv();
-    echo "<br> Studenti <br>";
-    print_r($pred->getSpisakStudenata());
-endforeach;
-
-echo "<br> lista predmeta2: ";
-foreach($nizpredmeta2 as $pred):
-    echo "<br>Predmet: ".$pred->getNaziv();
-    echo "<br> Studenti <br>";
-    print_r($pred->getSpisakStudenata());
-endforeach;
-
-echo "<br>";
-
-echo Kontroler::prikaziTabelu($nizstudenata, ["status", "indeks", "ime", "prezime", "email", "sifra", "jmbg", "telefon", "tip"]);
+// echo Kontroler::prikaziTabelu($nizstudenata, ["status", "indeks", "ime", "prezime", "email", "sifra", "jmbg", "telefon", "tip"]);
 ?>
---
+++ 20Projekti/04DomaciFakultet/loaddata.php
@@ -0,0 +1,50 @@
+<?php
+include "utility/obrada.php";
+include "model/korisnik.php";
+include "model/student.php";
+include "model/ostudent.php";
+include "model/mstudent.php";
+include "model/dstudent.php";
+include "model/profesor.php";
+include "model/administrator.php";
+include "model/predmet.php";
+include "model/ocena.php";
+
+if(!isset($_SESSION))
+session_start();
+
+$so = new OStudent("tamara", "naumovic", "tamara@elab.rs", "tamara123", "1234567891234", "0662581477", "student", "2020/1005");
+$sm = new MStudent("petar", "lukovac", "petar@elab.rs", "petar123", "1231234891234", "0661281477", "student", "2020/2005");
+$sd = new DStudent("aleksa", "miletic", "aleksaa@elab.rs", "aleksa123", "1234561231234", "0662123477", "student", "2020/3005");
+
+$prof1 = new Profesor("zorica", "bogdanovic", "zorica@elab.rs", "zorica123", "7894561231234", "0662123789", "profesor");
+$prof2 = new Profesor("dusan", "barac", "dusan@elab.rs", "dusan123", "7894333231234", "0662133789", "profesor");
+$prof3 = new Profesor("aleksandra", "labus", "aleksandra@elab.rs", "aleksandra123", "7894555231234", "0665523789", "profesor");
+
+$nizkorisnika = [$so, $sm, $sd, $prof1, $prof2, $prof3];
+$nizprof = [$prof1, $prof2, $prof3];
+
+$p1 = new Predmet("PHP", "php2022", "osnovne");
+$p2 = new Predmet("Baze", "baze2022", "osnovne");
+$p3 = new Predmet("JAVA", "java2022", "osnovne");
+$p4 = new Predmet("Blockchain", "bc2022", "doktorske");
+$p5 = new Predmet("IoT", "iot2022", "doktorske");
+$p6 = new Predmet("Internet tehnologije", "iteh2022", "doktorske");
+$p7 = new Predmet("Internet marketing", "imar2022", "master");
+$p8 = new Predmet("Mobilno poslovanje", "mpos2022", "master");
+
+$p1->setSpisakProfesora([$prof1]);
+$p2->setSpisakProfesora([$prof1]);
+$p3->setSpisakProfesora([$prof1]);
+$p4->setSpisakProfesora([$prof2]);
+$p5->setSpisakProfesora([$prof2]);
+$p6->setSpisakProfesora([$prof2]);
+$p7->setSpisakProfesora([$prof3]);
+$p8->setSpisakProfesora([$prof3]);
+
+$nizpredmeta = [$p1, $p2, $p3, $p4, $p5, $p6, $p7, $p8];
+
+$_SESSION["predmeti"] = $nizpredmeta;
+$_SESSION["users"] = $nizkorisnika;
+
+?>
