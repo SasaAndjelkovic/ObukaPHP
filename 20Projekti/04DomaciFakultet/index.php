1f64a0ac5f4a016780641f6d9b315f08ba35dcc9
SasaAndjelkovic <sandjelkovickg@gmail.com>
2022-12-26 23:35:18 +0100

    Zavrsen routhing

--- 20Projekti/04DomaciFakultet/index.php
+++ 20Projekti/04DomaciFakultet/index.php
@@ -1,13 +1,4 @@
 <?php
-include "model/korisnik.php";
-include "model/student.php";
-include "model/ostudent.php";
-include "model/mstudent.php";
-include "model/dstudent.php";
-include "model/profesor.php";
-include "model/administrator.php";
-include "model/predmet.php";
-include "model/ocena.php";
 include "controler/controler.php";
 include "loaddata.php";
 
