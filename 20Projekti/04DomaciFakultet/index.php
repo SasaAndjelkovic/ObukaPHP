6b9cf8aaf9fad00d056d4f05dc41fbc7f70f343d
SasaAndjelkovic <sandjelkovickg@gmail.com>
2022-12-26 22:13:55 +0100

    kvalitet logovanja

--- 20Projekti/04DomaciFakultet/index.php
+++ 20Projekti/04DomaciFakultet/index.php
@@ -12,7 +12,25 @@ include "controler/controler.php";
 include "loaddata.php";
 
 
-include "view/login.php";
+if(!isset($_SESSION["logovani_korisnik"])){
+    header("Location: view/login.php");
+}else{
+    $korisnik = $_SESSION["logovani_korisnik"];
+    include "view/header.php";
+    if($korisnik->getTip()=="student"){
+        include "view/student.php";
+        exit();
+    }else if($korisnik->getTip()=="profesor"){
+        include "view/profesor.php";
+        exit();
+    }else if($korisnik->getTip()=="administrator"){
+        include "view/administrator.php";
+        exit();
+    }else{
+        echo "404";
+        exit();
+    }
+}
 
 // sve je prebaceno u loaddata.php
 
