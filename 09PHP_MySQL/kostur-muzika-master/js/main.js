88ce2dc3c2fa75a8c4a502a8ed20ebeedf79d5dd
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-02-13 19:19:09 +0100

    javascript za instrument controler

---
+++ 09PHP_MySQL/kostur-muzika-master/js/main.js
@@ -0,0 +1,24 @@
+$("#dodajInstrument").submit(function(event){
+    event.preventDefault();
+
+    //citanje podataka
+    const $form = $(this);
+    const serializacija = $form.serialize();
+    console.log(serializacija);
+
+    //slanje zahteva
+    request = $.ajax({
+        url:"contoller/dodajInstrument.php",
+        type: "post",
+        data: serializacija
+    });
+
+    request.done(function(response, txtStatus, jqXHR){
+        if(response === 'Success'){
+            alert('Instrument je dodat');
+            location.reload();
+        } else {
+            console.log("Instrument nije dodat" + response);
+        }
+    });
+});
